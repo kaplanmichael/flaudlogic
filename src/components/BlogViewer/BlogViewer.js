b47647b67c6cc3286db70b23a6b8bad07c29a434
unknown <michael@michaelkaplan.net>
2020-06-06 18:09:06 -0400

    Fix broken Next button. Todo: Fix Prev button.

--- src/components/BlogViewer/BlogViewer.js
+++ src/components/BlogViewer/BlogViewer.js
@@ -1,40 +1,61 @@
 import React, { useState, useEffect } from 'react';
-function BlogViewerX(props) {
+const BlogViewerX = () => {
   const [data, setData] = useState([]);
   const [pagePos, setPagePos] = useState(0);
-  const [pageTokens, setpageTokens] = useState(['']);
+  const [pageTokens, setPageTokens] = useState(['']);
+  const [apiUrl, setApiUrl] = useState('https://www.googleapis.com/blogger/v3/blogs/2726861849607401146/posts?key=AIzaSyCgg1t9I1o3nz8OG1En1Bbqt8I-vF18LvU');
 
+  const createMarkup = (content) => {
+      return {__html: content}
+  }
+
+  const grabDate = (utc) => {
+    let date = new Date(utc);
+    return (`${date.getMonth()+1}.${date.getDate()}.${date.getFullYear()}`);
+  }
+
+  const grabTime = (utc) => {
+    let date = new Date(utc);
+    let hours = date.getHours();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = (hours % 12) || 12;
+    return (`${hours}:${date.getMinutes().toString().padStart(2, '0')} ${ampm}`);
+  }
+
+  const updateApiUrl = (pageToken) => {
+    if (pageToken !== undefined && pageToken !== '' ) {
+        let newUrl = apiUrl;
+        newUrl += '&pageToken=' + pageToken;
+        setApiUrl(newUrl);
+    }
+  }
+
+  const fetchBlogData = (pageToken) => {
+    console.log('loading data from ', apiUrl);
+    fetch(apiUrl)
+    .then(res => res.json())
+    .then(
+        result => {
+          console.log("result", (result));
+          setData(result)
+          //add new nextPage Token to the array of tokens
+          if(!pageTokens.includes(result.nextPageToken)) {
+              console.log('fetch: here is the loaded token ' + result.nextPageToken);
+              setPageTokens(pageTokens => [...pageTokens, result.nextPageToken]);
+          }
+        }
+    )
+  }
+
+  //useEffect(fetchBlogData, [])
+  useEffect(() => console.log('fetch: here it is pushed into the array ' + pageTokens), [pageTokens])
   useEffect(() => {
-        fetch("https://www.googleapis.com/blogger/v3/blogs/2726861849607401146/posts?key=AIzaSyCgg1t9I1o3nz8OG1En1Bbqt8I-vF18LvU")
-        .then(res => res.json())
-        .then(
-            result => {
-              console.log("result", (result));
-              setData(result)
-              //add new nextPage Token to the array of tokens
-              if(!pageTokens.includes(result.nextPageToken)) {
-                  console.log('fetch: here is the loaded token ' + result.nextPageToken);
-                  setpageTokens(pageTokens => [...pageTokens, result.nextPageToken]);
-                  console.log('fetch: here it is pushed into the array ' + pageTokens);
-              }
-            }
-        )
-    }, [])
-    const createMarkup = (content) => {
-        return {__html: content}
-    }
-    const grabDate = (utc) => {
-      let date = new Date(utc);
-      return (`${date.getMonth()+1}.${date.getDate()}.${date.getFullYear()}`);
-    }
-
-    const grabTime = (utc) => {
-      let date = new Date(utc);
-      let hours = date.getHours();
-      let ampm = hours >= 12 ? 'PM' : 'AM';
-      hours = (hours % 12) || 12;
-      return (`${hours}:${date.getMinutes().toString().padStart(2, '0')} ${ampm}`);
-    }
+    console.log('page pos was updated');
+    updateApiUrl(pageTokens[pagePos]);
+  }, [pagePos])
+  useEffect(() => {
+    fetchBlogData(pageTokens[pagePos]);
+  }, [apiUrl])
 
   return (
     <section className="blog-viewer">
@@ -42,15 +63,16 @@ function BlogViewerX(props) {
             <button onClick={() => {
               setPagePos(pagePos - 1);
               console.log('prev: heres where we are going', pageTokens[pagePos]);
-              props.fetch(pageTokens[pagePos]);
+              //fetchBlogData(pageTokens[pagePos]);
             }
         }>Prev</button>
     }
     {data.items && data.items.length === 10 && (
-        <button onClick={(props) => {
+        <button onClick={() => {
             setPagePos(pagePos + 1);
-            console.log('next: heres where we are going',pageTokens, pagePos, props);
-            props.fetch(pageTokens[pagePos]);
+            console.log('next: heres where we are going',pageTokens[pagePos]);
+            //fetchBlogData(pageTokens[pagePos]);
+
         }
         }>Next</button>
     )}
@@ -69,132 +91,3 @@ function BlogViewerX(props) {
 }
 
 export { BlogViewerX };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class BlogViewer extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            pagePos: 1
-        };
-    }
-
-    grabDate(utc) {
-      let date = new Date(utc);
-      return (`${date.getMonth()+1}.${date.getDate()}.${date.getFullYear()}`);
-    }
-
-    grabTime(utc) {
-      let date = new Date(utc);
-      let hours = date.getHours();
-      let ampm = hours >= 12 ? 'PM' : 'AM';
-      hours = (hours % 12) || 12;
-      return (`${hours}:${date.getMinutes().toString().padStart(2, '0')} ${ampm}`);
-    }
-
-    render() {
-        let blogEntries;
-        if (Object.keys(this.props.data).length !== 0) {
-            blogEntries = this.props.data.items.map(item => (
-                <article key={item.id}>
-                    <time><em>{`${this.grabDate(item.published)} // posted by ${item.author.displayName} @ ${this.grabTime(item.published)}`}</em></time>
-                    <h2>{item.title}</h2>
-                    <div dangerouslySetInnerHTML={this.createMarkup(item.content)}></div>
-                    [sharing widget]
-                </article>
-            ));
-
-
-        } else {
-            blogEntries = 'no data.';
-        }
-
-
-
-        return (
-            <section className="blog-viewer">
-                {/*
-                curr pos {this.state.pagePos}<br />
-                token at pos {this.state.pagePos-1} {this.props.pageTokens[this.state.pagePos-1]}<br />
-                token at curr pos {this.props.pageTokens[this.state.pagePos]}
-                <br />token at pos {this.state.pagePos+1} {typeof this.props.pageTokens[this.state.pagePos+1]}<br />
-                */}
-
-                {this.props.pageTokens[this.state.pagePos - 1] &&
-                        <button onClick={() => {
-                          this.setState( state => {
-                            return {
-                              pagePos: state.pagePos--
-                            }
-                          })
-                        console.log('prev: heres where we are going', this.props.pageTokens[this.state.pagePos]);
-                        this.props.fetch(this.props.pageTokens[this.state.pagePos]);
-                    }
-                    }>Prev</button>
-                }
-
-                {this.props.data.items && this.props.data.items.length === 10 && (
-                    <button onClick={() => {
-                        this.setState( state => {
-                          return {
-                            pagePos: state.pagePos++
-                          }
-                        })
-                        console.log('next: heres where we are going',this.props.pageTokens[this.state.pagePos]);
-                        this.props.fetch(this.props.pageTokens[this.state.pagePos]);
-                    }
-                    }>Next</button>
-                )}
-
-                <section>
-                    {blogEntries}
-                </section>
-
-                {this.props.pageTokens[this.state.pagePos] &&
-                        <button onClick={() => {
-                        this.state.pagePos--;
-                        console.log('prev: heres where we are going', this.props.pageTokens[this.state.pagePos]);
-                        this.props.fetch(this.props.pageTokens[this.state.pagePos]);
-                    }
-                    }>Prev</button>
-                }
-
-                {this.props.data.items && this.props.data.items.length === 10 && (
-                    <button onClick={() => {
-                        this.state.pagePos++;
-                        console.log('next: heres where we are going',this.props.pageTokens[this.state.pagePos]);
-                        this.props.fetch(this.props.pageTokens[this.state.pagePos]);
-                    }
-                    }>Next</button>
-                )}
-
-
-            </section>
-        );
-
-    }
-
-    createMarkup(content) {
-        return {__html: content};
-    }
-}
-
-export { BlogViewer }
-//todo: fix bug where prev button disappears if you leave the page
