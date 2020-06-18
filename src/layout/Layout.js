47b5146536cb71343b1f673875daba13cc443a13
unknown <michael@michaelkaplan.net>
2020-06-18 17:16:11 -0400

    Enable support of page-specific styles.

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -11,7 +11,7 @@ export default class Layout extends React.Component {
 
     render() {
         return (
-            <div id="content-container">
+            <div id="content-container" className={this.props.pageClass}>
                 <Header>
                     <Analytics />
                     <Navigation />
