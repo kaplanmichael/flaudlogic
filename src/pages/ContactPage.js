406c41f45f544dc68ff26e6a71f03c26f59423a5
unknown <michael@michaelkaplan.net>
2020-06-26 17:19:08 -0400

    Add temporary content for contact page.

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -6,15 +6,7 @@ export default class ContactPage extends React.Component {
         return (
             <Layout>
                 <h1>Contact</h1>
-                <h2>Mailing List</h2>
-                {/* <!-- // MAILCHIMP SUBSCRIBE CODE \\ --> */}
-                <p><a href="http://eepurl.com/ug2Y9">Subscribe</a> to the Flaud Logic Mailing List!</p>
-                {/* <!-- \\ MAILCHIMP SUBSCRIBE LINK // --> */}
-                <h2>Contact Flaud Logic</h2>
-                <p>Please use the form below to get in contact with Flaud Logic management.</p>
-
-
-
+                <p>-----@-----.---</p>
             </Layout>
         )
     }
