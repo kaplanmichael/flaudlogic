442e83cc430346bd41e5f12eb3cb0ef6c57ccfbc
unknown <michael@michaelkaplan.net>
2020-07-02 19:24:17 -0400

    Add new social media icon component to splash page and finish converting all of the splash page content to HTML text instead of it being part of the bkgd image.

--- src/pages/SplashPage.js
+++ src/pages/SplashPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import SplashLayout from '../layout/SplashLayout';
 import ReactGA from 'react-ga';
+import SocialMediaIcons from '../components/common/SocialMediaIcons';
+import SplashImage from '../images/art_fl_splash.jpg';
 
 export default class SplashPage extends React.Component {
 	render() {
@@ -9,7 +11,9 @@ export default class SplashPage extends React.Component {
             <SplashLayout>
 							<section className="splash-container">
 								<figure>
-									<img src={ require('../images/art_fl_splash.jpg') } width={700} height={632} useMap="#splash" alt="Flaud Logic | The Official Webscape" />
+									<NavLink to='/home'>
+										<img src={SplashImage} alt="Flaud Logic | The Official Webscape" />
+									</NavLink>
 									<figcaption>
 										<h1>Enter Now</h1>
 										<h4>The Official Webscape</h4>
@@ -24,22 +28,12 @@ export default class SplashPage extends React.Component {
 												Gustavo Sazes
 											</ReactGA.OutboundLink>.
 										</div>
+										<aside className="social">
+											<p>The Official Channels</p>
+											<SocialMediaIcons height={20}/>
+										</aside>
 									</figcaption>
 								</figure>
-
-								<map name="splash" id="splash">
-									<Link to="/home">
-										<area alt="" coords="77,65,624,413"/>
-									</Link>
-									<Link to="/home">
-										<area alt="" coords="162,413,530,485" />
-									</Link>
-									<area coords="376,553,424,577" href="http://www.youtube.com/user/FlaudLogic" target="_blank" alt="YouTube Channel" title="YouTube Channel" rel="noopener noreferrer"/>
-									<area coords="477,553,544,577" href="http://twitter.com/flaudlogic" target="_blank" alt="Twitter" title="Twitter" rel="noopener noreferrer"/>
-									<area coords="425,553,453,577" href="http://news.flaudlogic.com/feeds/posts/default?alt=rss" target="_blank" alt="RSS" title="RSS" rel="noopener noreferrer"/>
-									<area alt="" coords="570,525,646,539" href="https://gustavosazes.com/" target="_blank" rel="noopener noreferrer"/>
-									<area coords="453,553,477,577" href="http://www.facebook.com/flaudlogic" target="_blank" alt="Facebook Page" title="Facebook Page" rel="noopener noreferrer"/>
-								</map>
 							</section>
             </SplashLayout>
         )
