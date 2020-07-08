f641896560651905ef26ca60f94506194761c598
unknown <michael@michaelkaplan.net>
2020-07-08 13:33:03 -0400

    Code cleanup and small style fixes.

--- src/pages/LinksPage.js
+++ src/pages/LinksPage.js
@@ -6,38 +6,38 @@ export default class LinksPage extends React.Component {
     render() {
         return (
             <Layout>
-				<div className="links-page">
-					<h1>Links</h1>
-					<LinkCluster title="Flaud Logic Social Media Links">
-						<a href="http://www.youtube.com/user/FlaudLogic" title="YouTube Channel">YouTube Channel</a>
-						<a href="http://twitter.com/flaudlogic" title="Twitter">Twitter</a>
-						<a href="http://www.facebook.com/flaudlogic" title="Facebook Page">Facebook</a>
-						<a href="http://news.flaudlogic.com/feeds/posts/default?alt=rss" target="_blank" rel="noopener noreferrer" title="RSS">RSS Feed</a>
-					</LinkCluster>
-					<LinkCluster title="Flaud Logic Contributors">
-						<a href="http://www.bennylackner.com/" target="_blank" rel="noopener noreferrer">Benny Lackner</a>
-						<a href="http://www.amyward.com/" target="_blank" rel="noopener noreferrer">Amy Ward</a>
-						<a href="http://www.joebergamini.com/" target="_blank" rel="noopener noreferrer">Joe Bergamini</a>
-						<a href="http://www.scottthomasf.com/" target="_blank" rel="noopener noreferrer">Scott Thomas Ferreira</a>
-						<a href="http://www.stevenkaplan.com/" target="_blank" rel="noopener noreferrer">Steven Kaplan</a>
-						<a href="http://www.traxeast.com/" target="_blank" rel="noopener noreferrer">Trax East Recording Studio</a>
-					</LinkCluster>
-					<LinkCluster title="Related Bands, and Projects">
-						<a href="http://www.symphonyx.com" target="_blank" rel="noopener noreferrer">Symphony X</a>
-						<a href="http://www.theaterofthemindopera.com" target="_blank" rel="noopener noreferrer">Theater of the Mind Rock Opera</a>
-						<a href="http://www.wickedmonkey.net" target="_blank" rel="noopener noreferrer">Wicked Monkey</a>
-						<a href="http://www.oblivionsun.com/" target="_blank" rel="noopener noreferrer">Oblivion Sun</a>
-						<a href="http://www.happytheman.com/home.htm" target="_blank" rel="noopener noreferrer">Happy the Man</a>
-					</LinkCluster>
-					<LinkCluster title="Media Outlets">
-						<a href="http://www.progulus.com" target="_blank" rel="noopener noreferrer">Progulus Internet Radio</a>
-						<a href="http://www.beyondearcandy.com" target="_blank" rel="noopener noreferrer">Beyond Ear Candy</a>
-						<a href="http://www.ladyobscure.com" target="_blank" rel="noopener noreferrer">Lady Obscure</a>
-						<a href="http://www.progmetalzone.com" target="_blank" rel="noopener noreferrer">Prog Metal Zone</a>
-						<a href="http://www.progpalaceradio.com" target="_blank" rel="noopener noreferrer">Prog Palace Radio</a>
-						<a href="http://www.progrock.com" target="_blank" rel="noopener noreferrer">Progrock.com Radio</a>
-					</LinkCluster>
-				</div>
+      				<div className="links-page">
+      					<h1>Links</h1>
+      					<LinkCluster title="Flaud Logic Social Media Links">
+      						<a href="http://www.youtube.com/user/FlaudLogic" title="YouTube Channel">YouTube Channel</a>
+      						<a href="http://twitter.com/flaudlogic" title="Twitter">Twitter</a>
+      						<a href="http://www.facebook.com/flaudlogic" title="Facebook Page">Facebook</a>
+      						<a href="http://news.flaudlogic.com/feeds/posts/default?alt=rss" target="_blank" rel="noopener noreferrer" title="RSS">RSS Feed</a>
+      					</LinkCluster>
+      					<LinkCluster title="Flaud Logic Contributors">
+      						<a href="http://www.bennylackner.com/" target="_blank" rel="noopener noreferrer">Benny Lackner</a>
+      						<a href="http://www.amyward.com/" target="_blank" rel="noopener noreferrer">Amy Ward</a>
+      						<a href="http://www.joebergamini.com/" target="_blank" rel="noopener noreferrer">Joe Bergamini</a>
+      						<a href="http://www.scottthomasf.com/" target="_blank" rel="noopener noreferrer">Scott Thomas Ferreira</a>
+      						<a href="http://www.stevenkaplan.com/" target="_blank" rel="noopener noreferrer">Steven Kaplan</a>
+      						<a href="http://www.traxeast.com/" target="_blank" rel="noopener noreferrer">Trax East Recording Studio</a>
+      					</LinkCluster>
+      					<LinkCluster title="Related Bands, and Projects">
+      						<a href="http://www.symphonyx.com" target="_blank" rel="noopener noreferrer">Symphony X</a>
+      						<a href="http://www.theaterofthemindopera.com" target="_blank" rel="noopener noreferrer">Theater of the Mind Rock Opera</a>
+      						<a href="http://www.wickedmonkey.net" target="_blank" rel="noopener noreferrer">Wicked Monkey</a>
+      						<a href="http://www.oblivionsun.com/" target="_blank" rel="noopener noreferrer">Oblivion Sun</a>
+      						<a href="http://www.happytheman.com/home.htm" target="_blank" rel="noopener noreferrer">Happy the Man</a>
+      					</LinkCluster>
+      					<LinkCluster title="Media Outlets">
+      						<a href="http://www.progulus.com" target="_blank" rel="noopener noreferrer">Progulus Internet Radio</a>
+      						<a href="http://www.beyondearcandy.com" target="_blank" rel="noopener noreferrer">Beyond Ear Candy</a>
+      						<a href="http://www.ladyobscure.com" target="_blank" rel="noopener noreferrer">Lady Obscure</a>
+      						<a href="http://www.progmetalzone.com" target="_blank" rel="noopener noreferrer">Prog Metal Zone</a>
+      						<a href="http://www.progpalaceradio.com" target="_blank" rel="noopener noreferrer">Prog Palace Radio</a>
+      						<a href="http://www.progrock.com" target="_blank" rel="noopener noreferrer">Progrock.com Radio</a>
+      					</LinkCluster>
+      				</div>
             </Layout>
         )
     }
--- src/pages/PressPage.js
+++ src/pages/PressPage.js
@@ -18,50 +18,68 @@ export default class PressPage extends React.Component {
     render() {
         return (
             <Layout>
-				<section className="media-page">
-					<h1>Press</h1>
-          <h3>Press Kit</h3>
-					<p><a href={pressKit} target="_blank" rel="noopener noreferrer" onClick={()=> {
-            ReactGA.event({
-              category: 'User',
-              action: 'View',
-              label: 'Press Kit'
-            });
-          }}>View / Download</a>&nbsp;(PDF)</p>
-          <h3>Press Release</h3>
-          <p><a href={pressRelease} target="_blank" rel="noopener noreferrer" onClick={()=> {
-            ReactGA.event({
-              category: 'User',
-              action: 'View',
-              label: 'Press Release'
-            });
-          }}>Flaud Logic reveals album's special guest: Symphony X guitarist Michael Romeo</a>&nbsp;(January 17, 2013)</p>
-          <h3>Interview</h3>
-          <p><ReactGA.OutboundLink
-            eventLabel="Lady Obscure - Interview"
-            to="https://ladyobscure.com/http-www-ladyobscure-com-p5370-2/"
-            target="_blank"
-          >
-            Lady Obscure Music Magazine
-          </ReactGA.OutboundLink>
-          &nbsp;(March 2, 2013)</p>
-          <h2>Reviews</h2>
-					<p>"[The album is] awesome...Killer production. Man, when is the live concert in Berlin?"</p>
-					<p>— Benny Lackner,  Pianist, Composer</p>
-					<p>"...I have listened to it several times from start to finish, and I'm really digging it.  It gets better every time which says a lot about the depth of the work.  Takes me back to the days when I was obsessed with Tales From Topographic Oceans.  [Flaud Logic] is actually even more dynamic and moving on many fronts &#151; certainly more modern.  The production sounds great — the drums, variety of ripping guitar sounds, and vocals.  There is not much out there like this, so I'm so glad [it is] filling a void."</p>
-					<p>— Chris Rich,  Composer, Theater of the Mind Rock Opera</p>
-					<p>"Man, I'm looking forward to more stuff in the future from [Michael Kaplan]...This is a very, very impressive album...It is some tremendous stuff, in my opinion."</p>
-					<p>— Frank Remington, Prog Palace Radio</p>
-					<p>
-						<a href="http://prognaut.com/reviews/flaud-logic.html" target="_blank" rel="noopener noreferrer">Prognaut</a>&nbsp;(August 5, 2014)<br />
-						<a href="http://www.dprp.net/reviews/201416.php#flaudlogic" target="_blank" rel="noopener noreferrer">DPRP – Dutch Progressive Rock Page</a>&nbsp;(April , 2014)<br />
-						<a href="http://www.progmetalzone.com/2013/flaud-logic-review-fantastic-prog-rockmetal-debut-from-new-york-composer-and-musician-michael-kaplan/" target="_blank" rel="noopener noreferrer">Prog Metal Zone</a>&nbsp;(March 29, 2013)<br />
-						<a href="https://ladyobscure.com/http-www-ladyobscure-com-post_typealbumsp4815/" target="_blank" rel="noopener noreferrer">Lady Obscure Music Magazine</a>&nbsp;(March 2, 2013)<br />
-					</p>
-					<h2>Images</h2>
-					<p><span>Click image to enlarge.</span></p>
-					<DynamicImageGrid data={press}/>
-				</section>
+      				<section className="press-page">
+      					<h1>Press</h1>
+                <h3>Press Kit</h3>
+      					<p><a href={pressKit} target="_blank" rel="noopener noreferrer" onClick={()=> {
+                  ReactGA.event({
+                    category: 'User',
+                    action: 'View',
+                    label: 'Press Kit'
+                  });
+                }}>View / Download</a>&nbsp;(PDF)</p>
+                <h3>Press Release</h3>
+                <p><a href={pressRelease} target="_blank" rel="noopener noreferrer" onClick={()=> {
+                  ReactGA.event({
+                    category: 'User',
+                    action: 'View',
+                    label: 'Press Release'
+                  });
+                }}>Flaud Logic reveals album's special guest: Symphony X guitarist Michael Romeo</a>&nbsp;(January 17, 2013)</p>
+                <h3>Interview</h3>
+                <p><ReactGA.OutboundLink
+                  eventLabel="Lady Obscure - Interview"
+                  to="https://ladyobscure.com/http-www-ladyobscure-com-p5370-2/"
+                  target="_blank"
+                >
+                  Lady Obscure Music Magazine
+                </ReactGA.OutboundLink>
+                &nbsp;(March 2, 2013)</p>
+                <h2>Reviews</h2>
+      					<blockquote>"[The album is] awesome...Killer production. Man, when is the live concert in Berlin?"
+      					     <em>— Benny Lackner,  Pianist, Composer</em></blockquote>
+      					<blockquote>"...I have listened to it several times from start to finish, and I'm really digging it.  It gets better every time which says a lot about the depth of the work.  Takes me back to the days when I was obsessed with Tales From Topographic Oceans.  [Flaud Logic] is actually even more dynamic and moving on many fronts &#151; certainly more modern.  The production sounds great — the drums, variety of ripping guitar sounds, and vocals.  There is not much out there like this, so I'm so glad [it is] filling a void."
+      					     <em>— Chris Rich,  Composer, Theater of the Mind Rock Opera</em></blockquote>
+      					<blockquote>"Man, I'm looking forward to more stuff in the future from [Michael Kaplan]...This is a very, very impressive album...It is some tremendous stuff, in my opinion."
+      					     <em>— Frank Remington, Prog Palace Radio</em></blockquote>
+                <p>
+                  <ReactGA.OutboundLink
+                    eventLabel="Review - Prognaut"
+                    to="http://prognaut.com/reviews/flaud-logic.html"
+                    target="_blank"
+                  >
+                    Prognaut
+                  </ReactGA.OutboundLink>&nbsp;(August 5, 2014)<br />
+                  <ReactGA.OutboundLink
+                    eventLabel="Review - DPRP"
+                    to="http://www.dprp.net/reviews/201416.php#flaudlogic"
+                    target="_blank"
+                  >
+                    DPRP – Dutch Progressive Rock Page
+                  </ReactGA.OutboundLink>&nbsp;(April , 2014)<br />
+      						<span>Prog Metal Zone &nbsp;(March 29, 2013) — <em>no longer online but can be read in the press kit above</em></span><br />
+                  <ReactGA.OutboundLink
+                    eventLabel="Review - Lady Obscure Music Magazine"
+                    to="https://ladyobscure.com/http-www-ladyobscure-com-post_typealbumsp4815/"
+                    target="_blank"
+                  >
+                    Lady Obscure Music Magazine
+                  </ReactGA.OutboundLink>&nbsp;(March 2, 2013)<br />
+      					</p>
+      					<h2>Images</h2>
+      					<p><span>Click image to enlarge.</span></p>
+      					<DynamicImageGrid data={press}/>
+      				</section>
             </Layout>
         )
     }
