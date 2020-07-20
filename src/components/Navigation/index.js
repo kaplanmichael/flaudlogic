81cda7f39090a6ae92868058f0b1dda03c7b0da7
unknown <michael@michaelkaplan.net>
2020-07-20 18:36:06 -0400

    Working on responsive design for header/navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,6 +6,11 @@ export default class Navigation extends React.Component {
     render() {
         return (
                 <nav>
+                <svg className="nav-trigger" viewBox="0 0 100 80" width="40" height="40">
+                  <rect width="100" height="10" rx="8"></rect>
+                  <rect y="30" width="100" height="10" rx="8"></rect>
+                  <rect y="60" width="100" height="10" rx="8"></rect>
+                </svg>
                     <ul className="nav-items">
                         <li><NavLink to='/home'>Home</NavLink></li>
                         <li><NavLink to='/blog'>Blog</NavLink></li>
