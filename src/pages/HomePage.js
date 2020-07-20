04c3084d4f4194e5f098485c9ca7b07a6d51a27e
unknown <michael@michaelkaplan.net>
2020-07-20 15:14:05 -0400

    Add React Player package to project for the purpose of displaying youtube videos and possibly audio in the future.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../layout/Layout';
 import { BlogViewerX } from '../components/BlogViewer/BlogViewer';
 import HomeCarousel from '../components/HomeCarousel';
+import ReactPlayer from 'react-player/youtube';
 
 export default class HomePage extends React.Component {
     render() {
@@ -9,6 +10,7 @@ export default class HomePage extends React.Component {
             <Layout pageClass="home-page">
                 <HomeCarousel />
                 <p>Additional content coming soon!</p>
+                <ReactPlayer url='https://youtu.be/vpTuSpUboxY' />
             </Layout>
         )
     }
