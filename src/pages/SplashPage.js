7f050b2f0e521c9b60d9463af385d8b453cf31bd
unknown <michael@michaelkaplan.net>
2020-07-24 17:19:08 -0400

    Make responsive version of splash page and update headers that were not using the correct Birch Std font and new stroke effect.

--- src/pages/SplashPage.js
+++ src/pages/SplashPage.js
@@ -4,6 +4,7 @@ import SplashLayout from '../layout/SplashLayout';
 import ReactGA from 'react-ga';
 import SocialMediaIcons from '../components/common/SocialMediaIcons';
 import SplashImage from '../images/art_fl_splash.jpg';
+import { MainPageHeader } from '../components/common/MainPageHeader';
 
 export default class SplashPage extends React.Component {
 	render() {
@@ -15,21 +16,25 @@ export default class SplashPage extends React.Component {
 										<img src={SplashImage} alt="Flaud Logic | The Official Webscape" />
 									</NavLink>
 									<figcaption>
-										<h1>Enter Now</h1>
+									<NavLink to='/home'>
+										<MainPageHeader text="enter now" />
+									</NavLink>
 										<h4>The Official Webscape</h4>
-										<div className="copyright">
-											Reproduction of material from any flaudlogic.com page without written permission from the author is strictly prohibited.<br />
-											©2018 Copyright Michael D. Kaplan. All rights reserved.  Website layout by &nbsp;
+										<section className="copyright">
+											<p>Reproduction of material from any flaudlogic.com page without written permission from the author is strictly prohibited.<br />
+											©2020 Copyright Michael D. Kaplan. All rights reserved.  Website layout by &nbsp;
 											<ReactGA.OutboundLink
 												eventLabel="Gustavo Sazes"
 												to="https://gustavosazes.com/"
 												target="_blank"
 											>
 												Gustavo Sazes
-											</ReactGA.OutboundLink>.
-										</div>
+											</ReactGA.OutboundLink>
+											. </p>
+												<Link to="/contact">Contact</Link>
+										</section>
 										<aside className="social">
-											<p>The Official Channels</p>
+											<p data-text="The Official Channels">The Official Channels</p>
 											<SocialMediaIcons />
 										</aside>
 									</figcaption>
