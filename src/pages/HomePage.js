c971bba24f9aef03dc5a91af675a3f297c5e4502
unknown <michael@michaelkaplan.net>
2020-07-24 19:12:54 -0400

    Add temporary responsive wrapper to video player to prevent breakage until i have the chance to work on the video component.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,13 +4,24 @@ import { BlogViewerX } from '../components/BlogViewer/BlogViewer';
 import HomeCarousel from '../components/HomeCarousel';
 import ReactPlayer from 'react-player/youtube';
 
+const stylez = {
+  position: 'relative',
+  paddingTop: '56.25%'
+}
+const hello = {
+  position: 'absolute',
+  top: 0,
+  left: 0
+}
 export default class HomePage extends React.Component {
     render() {
         return (
             <Layout pageClass="home-page">
                 <HomeCarousel />
                 <p>Additional content coming soon!</p>
-                <ReactPlayer url='https://youtu.be/vpTuSpUboxY' />
+                <section style={stylez}>
+                  <ReactPlayer url='https://youtu.be/vpTuSpUboxY' width='100%' height='100%' style={hello} />
+                </section>
             </Layout>
         )
     }
