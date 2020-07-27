05f46592f639ed91144e4872a7dc9c63fb78cef8
unknown <michael@michaelkaplan.net>
2020-07-27 18:54:03 -0400

    Add imports for two-col and video-player components.  References the new components in the homepage.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import Layout from '../layout/Layout';
+import { TwoCol } from '../components/TwoCol';
 import { BlogViewerX } from '../components/BlogViewer/BlogViewer';
 import HomeCarousel from '../components/HomeCarousel';
-import ReactPlayer from 'react-player/youtube';
+import { VideoPlayer } from '../components/VideoPlayer';
 
-const stylez = {
-  position: 'relative',
-  paddingTop: '56.25%'
-}
-const hello = {
-  position: 'absolute',
-  top: 0,
-  left: 0
-}
 export default class HomePage extends React.Component {
     render() {
         return (
             <Layout pageClass="home-page">
                 <HomeCarousel />
-                <p>Additional content coming soon!</p>
-                <section style={stylez}>
-                  <ReactPlayer url='https://youtu.be/vpTuSpUboxY' width='100%' height='100%' style={hello} />
-                </section>
+                <TwoCol
+                  left={
+                    <>
+                      <h2>Video</h2>
+                      <VideoPlayer />
+                    </>
+                  }
+                  right={
+                    <>
+                      <h2>Latest News</h2>
+                    </>
+                  }
+                />
+
+
             </Layout>
         )
     }
