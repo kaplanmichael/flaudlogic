4bfead9f6e57a50d99f9f27dc6f4ae56be059622
Michael <michael@michaelkaplan.net>
2020-08-19 15:29:31 -0400

    Apply responsive styling to twitter widget.

--- src/components/common/TwitterTimeline.js
+++ src/components/common/TwitterTimeline.js
@@ -4,21 +4,21 @@ import { Timeline } from 'react-twitter-widgets'
 export default class TwitterTimeline extends React.Component {
 	render () {
 		return (
-            <div>
-				<Timeline
-					dataSource={{
-					  sourceType: 'profile',
-					  screenName: 'flaudlogic'
-					}}
-					options={{
-					  username: 'flaudlogic',
-					  height: '600',
-					  theme: 'dark',
-					  width: '320'
-					}}
-					onLoad={() => console.log('Timeline is loaded!')}
-				  />
-            </div>
+            <aside class="comp-twitter-timeline">
+							<Timeline
+								dataSource={{
+								  sourceType: 'profile',
+								  screenName: 'flaudlogic'
+								}}
+								options={{
+								  username: 'flaudlogic',
+								  height: '600',
+								  theme: 'dark',
+								  width: '320'
+								}}
+								onLoad={() => console.log('Timeline is loaded!')}
+							  />
+            </aside>
         );
     }
 }
